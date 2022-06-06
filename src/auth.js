fb425c8dd4d425b2de1edb861056a72ce6377281
Jack <jack.stuart.braga@gmail.com>
2022-06-06 15:59:34 +1000

    Added Function authRegister

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,7 @@
 function authLoginV1 (email, password) {
 	return 'email' + 'password';
 }
+
+function authRegister V1 (email, password, first_name, last_name) {
+	return 'email' + 'password' + 'first_name' + 'last_name';
+}

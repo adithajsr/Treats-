f3bd9db805287ce6819dcb7d983d78f28afa2bc5
Caitlyn Wong <z5360361@unsw.edu.au>
2022-06-06 15:57:06 +1000

    Added stub for channelsCreateV1 function

--- src/channels.js
+++ src/channels.js
@@ -1,1 +1,7 @@
-
+// Sample stub for a function 'channelsCreateV1',
+// with arguments named 'authUserId', 'name' and 'isPublic'
+// Returns a string concatenating the name of 'authUserId', 'name' and 'isPublic'
+function channelsCreateV1(authUserId, name, isPublic) {
+    return 'authUserId' + 'name' + 'isPublic';
+}
+  

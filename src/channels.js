1d5ff05e197a0b96a803924ce1de2e7126560653
Rachel (Yu Kyung) <z5389100@ad.unsw.edu.au>
2022-06-09 01:57:13 +1000

    fixed function name

--- src/channels.js
+++ src/channels.js
@@ -5,6 +5,6 @@ function channelsCreateV1(authUserId, name, isPublic) {
     return 'authUserId' + 'name' + 'isPublic';
 }
 
-function channelsListV1(authUserId) {
-    return authUserId;
+function channelsListallV1(authUserId) {
+    return 'authUserId';
 }

2d6b3c7e16249f57d7d85062350449b63dde86cf
Rachel (Yu Kyung) <z5389100@ad.unsw.edu.au>
2022-06-09 01:54:02 +1000

    added quotation marks, resolved merge conflicts

--- src/channel.js
+++ src/channel.js
@@ -6,12 +6,10 @@ function channelInviteV1(authUserId, channelId, uId) {
   return 'authUserId' + 'channelId'+ 'uId';
 }
 
-<<<<<<< HEAD
 function channelDetailsV1(authUserId, channelId) {
   return 'authUserId' + 'channelId';
 }
-=======
+
 function ChannelMessagesV1(authUserId, channelId, start) {
   return 'authUserId' + 'channelId' + 'start';
 }
->>>>>>> c4caaa830d659d898157f28c65b9995c289ac165

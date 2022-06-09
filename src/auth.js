f21056e32315e46fe38fad09373c3c4776f5041a
Caitlyn Wong <z5360361@unsw.edu.au>
2022-06-09 12:11:04 +1000

    Changing parameters in authRegisterV1

--- src/auth.js
+++ src/auth.js
@@ -2,6 +2,6 @@ function authLoginV1 (email, password) {
 	return 'email' + 'password';
 }
 
-function authRegisterV1 (email, password, first_name, last_name) {
-	return 'email' + 'password' + 'first_name' + 'last_name';
+function authRegisterV1 (email, password, nameFirst, nameLast) {
+	return 'email' + 'password' + 'nameFirst' + 'nameLast';
 }

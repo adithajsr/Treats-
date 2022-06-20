42030329527bfc4b9cac1c17f3ad192c4fae77fe
Jack <jack.stuart.braga@gmail.com>
2022-06-21 00:00:51 +1000

    Completed a single test, installed uuid, planned to create functions that test the handle and uuid validity both through regex

--- src/auth.test.js
+++ src/auth.test.js
@@ -1,22 +1,28 @@
-import { authLoginV1, authRegisterV1 } from './auth';
+import { authLoginV1, authRegisterV1, isHandleValid, isUuidValid } from './auth';
+// import { clearV1 } from './other';
 import { validator } from 'validator';
 
 describe('Testing for authRegisterV1', () => {
     beforeEach(() => {
-      // clear();
+      // clearV1();
+      dataSet = getData();
     });
   
     afterAll(() => {
-      // clear();
+      // clearV1();
     });
   
-    test('Test 1', () => {
+    test('Test 1 affirmitive', () => {
+      // all should be well
         testUserEmail = 'who.is.joe@is.the.question.com';
         testUserPw = 'yourmumma'
         testUserId = authRegisterV1(testUserEmail, testUserPw);
-        expect(testUserId).toStrictEqual({
-            
-        });
+        expect(isUuidValid(testUserId)).toBe(true);
+        expect(isHandleValid(data.user[0].handle)).toBe(true);
+        expect(validator.isEmail(data.user[0].email)).toBe(true);
+        expect(data.user[0].password).not.toBe(null);
+        expect(data.user[0].nameFirst).not.toBe(null);
+        expect(data.user[0].nameLast).not.toBe(null);
     });
 
     test('Test 1', () => {

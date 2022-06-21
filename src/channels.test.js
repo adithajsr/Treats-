bf9415178e0e4af3e11df1859cbb7bbbd5535096
Caitlyn Wong <z5360361@unsw.edu.au>
2022-06-21 19:29:00 +1000

    Added test for successful channel creation

--- src/channels.test.js
+++ src/channels.test.js
@@ -1,15 +1,12 @@
 import { channelsCreateV1, channelsListV1, channelsListallV1 } from './channels';
 import { clearV1 } from './other';
+import { validate as uuidValidate } from 'uuid';
 
 // test('Test successful echo', () => {
 //   let result = echo('1');
 //   expect(result).toBe('1');
 //   result = echo('abc');
 //   expect(result).toBe('abc');
-// });
-
-// test('Test invalid echo', () => {
-//   expect(echo({ echo: 'echo' })).toStrictEqual({ error: 'error' });
 // });
 
 describe('channels capabilities', () => {
@@ -21,23 +18,35 @@ describe('channels capabilities', () => {
     });
 
     // Creates a new channel with the given name that is either a public
-    // or private channel. The user who created it automatically joins
-    // the channel.
+    // or private channel.
+    // The user who created it automatically joins the channel.
 
-    // Parameters: { authUserId, name, isPublic }
+    // Parameters: { authUserId (integer), name (string), isPublic (boolean) }
     // e.g. channelsCreateV1( 12, 'M13A_AERO', false )
 
-    // Return type if no error: { channelId }
+    // Return type if no error: { channelId (integer) }
 
-    test('fails on invalid name', () => {
+    test('test successful channel creation', () => {
+
+      let result = channelsCreateV1( 12, 'publicChannel', true );
+      expect(uuidValidate(String(result))).toBe(true);
+
+      result = channelsCreateV1( 12345, 'privateChannel', false );
+      expect(uuidValidate(String(result))).toBe(true);
+
+      // TODO: test if user who created it joined the channel?
+      
+    });
+
+    test('test invalid channel creation', () => {
 
       // length of name is less than 1 character
       expect(channelsCreateV1( 12, '', true )).toStrictEqual({ error: 'error' });
       expect(channelsCreateV1( 12345, '', false )).toStrictEqual({ error: 'error' });
 
       // length of name is more than 20 characters
-      expect(channelsCreateV1( 12, 'Morethantwentycharacters', true )).toStrictEqual({ error: 'error' });
-      expect(channelsCreateV1( 12345, 'Veryverylongchannelname', false )).toStrictEqual({ error: 'error' });
+      expect(channelsCreateV1( 12, 'moreThanTwentyCharacters', true )).toStrictEqual({ error: 'error' });
+      expect(channelsCreateV1( 12345, 'veryVeryLongChannelName', false )).toStrictEqual({ error: 'error' });
       
     });
 

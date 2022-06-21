4f0b789111246bae9aa50a77ca6aa0d16cf4a9b3
Rachel (Yu Kyung) <z5389100@ad.unsw.edu.au>
2022-06-22 00:19:47 +1000

    set up test files for channelDetailsV1 - disregard previous commits under same commit message, wrongly labelled

---
+++ src/channel.test.js
@@ -0,0 +1,4 @@
+import { channelJoinV1, channelInviteV1, channelDetailsV1, channelMessagesV1 } from './channel';
+import { clearV1 } from './other';
+import { validate as uuidValidate } from 'uuid';
+

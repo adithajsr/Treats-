98a7ccbe860e127ad0696d9aa16dcee9fa8273c4
Rachel (Yu Kyung) <z5389100@ad.unsw.edu.au>
2022-06-22 04:04:15 +1000

    edited tests for channelDetailsV1

--- src/channel.test.js
+++ src/channel.test.js
@@ -1,23 +1,12 @@
 import { channelJoinV1, channelInviteV1, channelDetailsV1, channelMessagesV1 } from './channel';
 import { clearV1 } from './other';
 import { validate as uuidValidate } from 'uuid';
-
-// return error when 
-// channelId doesn't refer to a valid channel
-// channelId is valid and the authorised user is not a member of the channel
+import { v4 } from "uuid";
 
 // return type if no error: name, isPublic, ownerMembers, allMembers
 
 // channelDetailsV1
 // Given a channel with ID channelId that the authorised user is a member of, provide basic details about the channel.
-
-
-// tests
-// ensure errors are returned for invalid channel
-// ensure errors are returned for authorised user isn't a member of the channel but channelId is valid
-// check that a valid channelId with an authorised user that is a member returns channel details
-
-
 
 describe (channelDetailsV1 check) {
 
@@ -39,12 +28,11 @@ describe (channelDetailsV1 check) {
     });
 
     test('test successful return', () => {
-
-        let result = channelDetailsV1()
-        expect().toEqual();
+        let authUserId = v4();
+        let result = channelDetailsV1(authUserId, );
+        expect(result).toEqual();
 
     });
-
 
 
 }

5aef9e8be1a983525ced28723c3816f7ca183dbc
Rachel (Yu Kyung) <z5389100@ad.unsw.edu.au>
2022-06-23 06:27:10 +1000

    making progress on tests for channelsListallV1

---
+++ src/channels.test.js
@@ -0,0 +1,38 @@
+import { channelsCreateV1, channelsListV1, channelsListallV1 } from './channels';
+import { clearV1 } from './other';
+import { validate as uuidValidate } from 'uuid';
+
+describe ('channelsListallV1 test', () => {
+
+    beforeEach(() => {
+        clearV1();
+      });
+
+    test('authid is invalid', () => {
+
+        expect(channelsListallV1('not a UUID')).toStrictEqual({ error: 'error' });
+
+    });
+
+    test('no channels in database', () => {
+
+        let authUserId = v4();
+        expect(channelsListallV1(authUserId)).toStrictEqual({ error: 'error' });
+
+    });
+
+    test('return one channel', () => {
+        
+        let authUserId = v4();
+        let channelInfo = {channelId: 1, name: 'apple', isPublic: false, ownerMembers: [1], allMembers: [1, 2, 3, 4]}
+        expect(channelsListallV1(authUserId)).toStrictEqual({
+            
+        });
+    });
+
+    test('return multiple channels', () => {
+        let authUserId = v4();
+        
+    });
+
+});

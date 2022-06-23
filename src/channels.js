4bb5badead811d60e9118f828227db58a6dbd147
Rachel (Yu Kyung) <z5389100@ad.unsw.edu.au>
2022-06-24 03:10:38 +1000

    added error checks and functionality to channelsListallV1 function

--- src/channels.js
+++ src/channels.js
@@ -1,19 +1,38 @@
+import { getData, setData } from './dataStore';
+
 function channelsCreateV1(authUserId, name, isPublic) {
     return {
       channelId: 1,
     };
   }
   
-  function channelsListV1(authUserId) {
-    return {
-      channels: [] // see interface for contents
-    };
+function channelsListV1(authUserId) {
+  return {
+    channels: [] // see interface for contents
+  };
+}
+
+function channelsListallV1(authUserId) {
+  const data = getData();
+  let foundChannels = [];
+
+  // invalid authUserId
+  if (isNaN(authUserId) === true) {
+    return { error: 'error' }
   }
-  
-  function channelsListallV1(authUserId) {
-    return {
-      channels: [{channelId: 123456, name: 'alphabet'}] // see interface for contents
-    };
+
+  // check if any channels in data
+  if (data.channel.length <= 0) {
+    return foundChannels;
   }
+
+  for (const i in data.channel) {
+    foundChannels.push({channelId: data.channel[i].channelId, name: data.channel[i].channelName});
+  }
+
+  return {
+    channels: foundChannels // see interface for contents
+  };
+}
   
-  export { channelsCreateV1, channelsListV1, channelsListallV1 };
+export { channelsCreateV1, channelsListV1, channelsListallV1 };

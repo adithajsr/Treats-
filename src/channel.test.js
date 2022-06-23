52ff5a5e88d48d9f3bf091efaa7c46aabfc503b2
Rachel (Yu Kyung) <z5389100@ad.unsw.edu.au>
2022-06-24 04:35:22 +1000

    modified tests for channelDetailsV1

--- src/channel.test.js
+++ src/channel.test.js
@@ -1,4 +1,6 @@
 import { channelJoinV1, channelInviteV1, channelDetailsV1, channelMessagesV1 } from './channel.js';
+import { channelsCreateV1 } from './channels.js'
+import { authRegisterV1 } from './auth.js'
 import { clearV1 } from './other.js';
 import { validate as uuidValidate, v4 } from 'uuid';
 
@@ -7,81 +9,40 @@ describe('channelDetailsV1 check', () => {
     beforeEach(() => {
         clearV1();
     });
+
+    // channelDetailsV1 { authUserId, channelId }
+    // returns { name, isPublic, ownerMembers, allMembers }
     
-    test('test error returned on invalid channel', () => {
+    test('test error returned on invalid channel - empty string', () => {
 
-        let result = channelDetailsV1(123, "");
-        expect(result).toStrictEqual({ error: 'error' });
-
-    });
-
-    test('test error returned on valid channel but non-member authorised user', () => {
-
-        expect(channelDetailsV1('not a UUID')).toStrictEqual({ error: 'error' });
+        expect(channelDetailsV1(123, "")).toStrictEqual({ error: 'error' });
 
     });
 
-    test('test successful return', () => {
-        let authUserId = v4();
-        let channelInfo = {
-            channelId: 1, 
-            name: 'apple', 
-            isPublic: false, 
-            ownerMembers: [
-                {
-                uId: 20,
-                email: 'johndoe@unsw.com',
-                nameFirst: 'john',
-                nameLast: 'doe',
-                handleStr: 'johnDoe0'
-                }
-            ], 
-            allMembers: [
-                {
-                uId: 20,
-                email: 'johndoe@unsw.com',
-                nameFirst: 'john',
-                nameLast: 'doe',
-                handleStr: 'johnDoe0'
-                },
-                {
-                uId: 50,
-                email: 'janedoe@unsw.com',
-                nameFirst: 'jane',
-                nameLast: 'doe',
-                handleStr: 'janeDoe0'
-                }
-            ]
-        }
-        expect(channelDetailsV1(authUserId, 1)).toStrictEqual({
-            name: 'apple', 
-            isPublic: false,
-            ownerMembers: [
-                {
-                uId: 20,
-                email: 'johndoe@unsw.com',
-                nameFirst: 'john',
-                nameLast: 'doe',
-                handleStr: 'johnDoe0'
-                }
-            ], 
-            allMembers: [
-                {
-                uId: 20,
-                email: 'johndoe@unsw.com',
-                nameFirst: 'john',
-                nameLast: 'doe',
-                handleStr: 'johnDoe0'
-                },
-                {
-                uId: 50,
-                email: 'janedoe@unsw.com',
-                nameFirst: 'jane',
-                nameLast: 'doe',
-                handleStr: 'janeDoe0'
-                }
-            ]
+    test('test error returned on invalid channel - not a number', () => {
+
+        expect(channelDetailsV1(123, "abcdef")).toStrictEqual({ error: 'error' });
+
+    });
+
+    test('test error returned on valid channel but authorised user is not a member of the channel', () => {
+        const c1 = channelsCreateV1(12345, 'channelone', true); 
+        expect(channelDetailsV1(999999, c1)).toStrictEqual({ error: 'error' });
+
+    });
+
+    test('test successful return - one channel,', () => {
+
+        const c1 = channelsCreateV1(12345, 'channelone', true); 
+        const a1 = authRegisterV1('email@unsw.com', 'password', 'john', 'doe')
+        channelJoin(a1, c1);
+        expect(channelDetailsV1(a1, c1)).toStrictEqual({
+            name: c1.channelName,
+            isPublic: c1.isPublic,
+            ownerMembers: c1.ownerMembers,
+            allMembers: c1.allMembers,
         });
+
     });
 
 

082239d71a7d77928341c46a9f11f663639484e1
Jack <jack.stuart.braga@gmail.com>
2022-06-23 23:00:57 +1000

    Completed Testing for authRegisterV1

--- src/auth.test.js
+++ src/auth.test.js
@@ -17,31 +17,58 @@ describe('Testing for authRegisterV1', () => {
       // all should be well
         testUserEmail = 'who.is.joe@is.the.question.com';
         testUserPw = 'yourmumma'
-        testUserId = authRegisterV1(testUserEmail, testUserPw);
+        testUserFN = 'John';
+        testUserLN = 'Smith';
+        testUserId = authRegisterV1(testUserEmail, testUserPw, testUserFN, testUserLN);
         expect(isUuidValid(testUserId)).toBe(true);
         expect(isHandleValid(data.user[0].handle)).toBe(true);
         expect(validator.isEmail(data.user[0].email)).toBe(true);
-        expect(data.user[0].password).not.toBe(null);
-        expect(data.user[0].nameFirst).not.toBe(null);
-        expect(data.user[0].nameLast).not.toBe(null);
+        expect(data.user[0].password).toBe('yourmumma');
+        expect(data.user[0].nameFirst).toBe('John');
+        expect(data.user[0].nameLast).toBe('Smith');
     });
 
     test('Test 2 invalid email', () => {
-      // all should be well
+      // invalid email - no '@' smymbol
         testUserEmail = 'z5420895$ad.unsw.edu.au';
         testUserPw = 'myrealpassword'
-        testUserId = authRegisterV1(testUserEmail, testUserPw);
+        testUserFN = 'Jonathan';
+        testUserLN = 'Schmidt';
+        testUserId = authRegisterV1(testUserEmail, testUserPw, testUserFN, testUserLN);
         expect(testUserId).toBe({ error: 'error'});
         expect(validator.isEmail(testUserEmail)).toBe(false);
     });
 
-    test('Test 1', () => {
-      //
-      expect(authRegisterV1(email, password)).toStrictEqual({ error: 'error' });
+    test('Test 3 invalid password', () => {
+      // invalid password - less than 5 characters
+        testUserEmail = 'i.love.to.code@university.com';
+        testUserPw = 'this5'
+        testUserFN = 'Jean';
+        testUserLN = 'McQueen';
+        testUserId = authRegisterV1(testUserEmail, testUserPw, testUserFN, testUserLN);
+        expect(testUserId).toBe({ error: 'error'});
+        expect(validator.isEmail(testUserEmail)).toBe(true);
     });
 
-    test('Test 1', () => {
-        //
-        expect(authRegisterV1(email, password)).toStrictEqual({ error: 'error' });
-      });
+    test('Test 4 invalid first name', () => {
+      // invalid nameFirst - below [1-50] range
+        testUserEmail = 'having.fun@writing.tests.com';
+        testUserPw = 'thispasswordislongenough'
+        testUserFN = '';
+        testUserLN = 'Tou';
+        testUserId = authRegisterV1(testUserEmail, testUserPw, testUserFN, testUserLN);
+        expect(testUserId).toBe({ error: 'error'});
+        expect(validator.isEmail(testUserEmail)).toBe(true);
+    });
+
+    test('Test 5 invalid last name', () => {
+      // invalid nameLast - above [1-50] range
+        testUserEmail = 'still.having.fun@writing.tests.com';
+        testUserPw = 'thispasswordismorethanlongenough'
+        testUserFN = 'Tim';
+        testUserLN = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'; // 52 characters long
+        testUserId = authRegisterV1(testUserEmail, testUserPw, testUserFN, testUserLN);
+        expect(testUserId).toBe({ error: 'error'});
+        expect(validator.isEmail(testUserEmail)).toBe(true);
+    });
 });

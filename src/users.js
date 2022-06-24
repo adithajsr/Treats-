654acfedb372e46d8668ec5143336c57f0d9b29c
Aditha Jayasuriya <z5363611@unsw.edu.au>
2022-06-24 17:41:29 +1000

    Added implementation for function users.js

--- src/users.js
+++ src/users.js
@@ -1,11 +1,25 @@
 function userProfileV1(authUserId, uId) {
-  return {
-    uId: 1, 
-    email: 'example@gmail.com',
-    nameFirst: 'Hayden', 
-    nameLast: 'Smith', 
-    handleStr: 'haydensmith'
-  }
-}
+	
+	//Determining whether authUserId is valid 
+	let count = 0;
+	for (const element in data.user) {
+		if (authUserId === data.user[element].uId) {
+			count = 1;
+			break;
+		}
+	}
 
+	//If invalid authUserId
+	if (count === 0) return {error: 'error'}
+
+
+	//Searching for the uId
+	for (const element in data.user) {
+		if (uId === data.user[element].uId) {
+			return user;
+		}
+	}
+	//If uId doesn't match any uId in data object
+	return {error: 'error'};
+}
 export { userProfileV1 }

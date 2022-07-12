d56de0fff536b761788a4993a6c6de042c37146d
Aditha Jayasuriya <z5363611@unsw.edu.au>
2022-07-12 22:39:44 +1000

    Changing files from .js to .ts

---
+++ src/other.ts
@@ -0,0 +1,17 @@
+
+
+import {getData} from './dataStore.js'
+import {setData} from './dataStore.js'
+
+//Assume that the clear function should keep the users and channels arrays and not 
+//remove them.
+export function clearV1() {
+	let data = getData();
+	
+	data.user = [];
+	data.channel = [];
+
+	setData(data);
+
+	return {};
+}

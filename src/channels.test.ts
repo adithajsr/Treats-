b6eb0af0dedd1bdc603ea3bb284734edadda3266
Caitlyn Wong <z5360361@unsw.edu.au>
2022-07-13 16:31:01 +1000

    Added helper functions to channels.test.ts and modified tests for /channels/list/v2

--- src/channels.test.ts
+++ src/channels.test.ts
@@ -5,8 +5,20 @@ const OK = 200;
 const port = config.port;
 const url = config.url;
 
-// TODO: potentially improve types
-type wrapperOutput = {
+// TODO: potentially replace any types
+type user = {
+  email: string,
+  password: string,
+  nameFirst: string,
+  nameLast: string,
+  res: any,
+  bodyObj: any,
+};
+
+type channel = {
+  token: string,
+  name: string,
+  isPublic: boolean,
   res: any,
   bodyObj: any,
 };
@@ -64,17 +76,44 @@ function requestClear() {
   return JSON.parse(String(res.getBody()));
 }
 
+const createTestUser = (email: string, password: string, nameFirst: string, nameLast: string) => {
+  // auth/register/v2 returns { token, authUserId }
+  const requestOutput = requestAuthRegister(email, password, nameFirst, nameLast);
+
+  return {
+    email: email,
+    password: password,
+    nameFirst: nameFirst,
+    nameLast: nameLast,
+    res: requestOutput.res,
+    bodyObj: requestOutput.bodyObj,
+  };
+};
+
+const createTestChannel = (token: string, name: string, isPublic: boolean) => {
+  // channels/create/v2 returns { channelId }
+  const requestOutput = requestChannelsCreate(token, name, isPublic);
+
+  return {
+    token: token,
+    name: name,
+    isPublic: isPublic,
+    res: requestOutput.res,
+    bodyObj: requestOutput.bodyObj,
+  };
+};
+
 beforeEach(() => {
   requestClear();
 });
 
 describe('channels capabilities', () => {
   describe('test /channels/create/v2', () => {
-    let testUser: wrapperOutput;
+    let testUser: user;
 
     beforeEach(() => {
       // Create a test user
-      testUser = requestAuthRegister('validemail@gmail.com', '123abc!@#', 'John', 'Doe');
+      testUser = createTestUser('validemail@gmail.com', '123abc!@#', 'John', 'Doe');
       expect(testUser.bodyObj).not.toStrictEqual({ error: 'error' });
     });
 
@@ -106,33 +145,33 @@ describe('channels capabilities', () => {
 
   describe('test /channels/list/v2', () => {
 
-    let testUser1: wrapperOutput;
-    let testUser2: wrapperOutput;
-    let testUser3: wrapperOutput;
-    let testUser4: wrapperOutput;
-    let testChannel1: wrapperOutput;
+    let testUser1: user;
+    let testUser2: user;
+    let testUser3: user;
+    let testUser4: user;
+    let testChannel1: channel;
 
     beforeEach(() => {
       // Create test user 1
-      testUser1 = requestAuthRegister('validemail@gmail.com', '123abc!@#', 'John', 'Doe');
+      testUser1 = createTestUser('validemail@gmail.com', '123abc!@#', 'John', 'Doe');
       expect(testUser1.bodyObj).not.toStrictEqual({ error: 'error' });
 
       // Create test user 2
-      testUser2 = requestAuthRegister('student@unsw.com', 'password', 'Jane', 'Schmoe');
+      testUser2 = createTestUser('student@unsw.com', 'password', 'Jane', 'Schmoe');
       expect(testUser2.bodyObj).not.toStrictEqual({ error: 'error' });
 
       // Create test user 3
-      testUser3 = requestAuthRegister('tsmith@yahoo.com', 'qwerty', 'Tom', 'Smith');
+      testUser3 = createTestUser('tsmith@yahoo.com', 'qwerty', 'Tom', 'Smith');
       expect(testUser3.bodyObj).not.toStrictEqual({ error: 'error' });
 
       // Create test user 4
-      testUser4 = requestAuthRegister('jbloggs@proton.com', '111111', 'Jo', 'Bloggs');
+      testUser4 = createTestUser('jbloggs@proton.com', '111111', 'Jo', 'Bloggs');
       expect(testUser4.bodyObj).not.toStrictEqual({ error: 'error' });
 
       // TODO: use channelJoin to add more people to channels
 
       // testUser1 created testChannel1 so they automatically join it
-      testChannel1 = requestChannelsCreate(testUser1.bodyObj.token, 'channelName', true);
+      testChannel1 = createTestChannel(testUser1.bodyObj.token, 'channelName', true);
       expect(testChannel1.bodyObj).not.toStrictEqual({ error: 'error' });
     });
 
@@ -152,7 +191,7 @@ describe('channels capabilities', () => {
         channels: [
           {
             channelId: testChannel1.bodyObj.channelId,
-            name: testChannel1.bodyObj.name,
+            name: testChannel1.name,
           }
         ]
       });
@@ -172,26 +211,26 @@ describe('channels capabilities', () => {
 
     test('Multiple channels, authorised user is in all channels', () => {
       // testUser1 is in all channels
-      const c1A = requestChannelsCreate(testUser1.bodyObj.token, 'channel1AName', false);
-      const c1B = requestChannelsCreate(testUser1.bodyObj.token, 'channel1BName', true);
-      const c1C = requestChannelsCreate(testUser1.bodyObj.token, 'channel1CName', false);
+      const c1A = createTestChannel(testUser1.bodyObj.token, 'channel1AName', false);
+      const c1B = createTestChannel(testUser1.bodyObj.token, 'channel1BName', true);
+      const c1C = createTestChannel(testUser1.bodyObj.token, 'channel1CName', false);
 
       const expected = new Set([
         {
           channelId: testChannel1.bodyObj.channelId,
-          name: testChannel1.bodyObj.name,
+          name: testChannel1.name,
         },
         {
           channelId: c1A.bodyObj.channelId,
-          name: c1A.bodyObj.name,
+          name: c1A.name,
         },
         {
           channelId: c1B.bodyObj.channelId,
-          name: c1B.bodyObj.name,
+          name: c1B.name,
         },
         {
           channelId: c1C.bodyObj.channelId,
-          name: c1C.bodyObj.name,
+          name: c1C.name,
         },
       ]);
 
@@ -204,18 +243,18 @@ describe('channels capabilities', () => {
 
     test('Multiple channels, authorised user is in some channels', () => {
       // testUser1 is in some channels, remaining channels created by testUser2
-      const c1A = requestChannelsCreate(testUser1.bodyObj.token, 'channel1AName', false);
-      const c2A = requestChannelsCreate(testUser2.bodyObj.token, 'channel2AName', true);
-      const c2B = requestChannelsCreate(testUser2.bodyObj.token, 'channel2BName', false);
+      const c1A = createTestChannel(testUser1.bodyObj.token, 'channel1AName', false);
+      const c2A = createTestChannel(testUser2.bodyObj.token, 'channel2AName', true);
+      const c2B = createTestChannel(testUser2.bodyObj.token, 'channel2BName', false);
 
       const expected = new Set([
         {
           channelId: testChannel1.bodyObj.channelId,
-          name: testChannel1.bodyObj.name,
+          name: testChannel1.name,
         },
         {
           channelId: c1A.bodyObj.channelId,
-          name: c1A.bodyObj.name,
+          name: c1A.name,
         },
       ]);
 
@@ -228,9 +267,9 @@ describe('channels capabilities', () => {
 
     test('Multiple channels, authorised user is in no channels', () => {
       // testUser2 is in no channels, all channels created by testUser1
-      const c1A = requestChannelsCreate(testUser1.bodyObj.token, 'channel1AName', false);
-      const c1B = requestChannelsCreate(testUser1.bodyObj.token, 'channel1BName', true);
-      const c1C = requestChannelsCreate(testUser1.bodyObj.token, 'channel1CName', false);
+      const c1A = createTestChannel(testUser1.bodyObj.token, 'channel1AName', false);
+      const c1B = createTestChannel(testUser1.bodyObj.token, 'channel1BName', true);
+      const c1C = createTestChannel(testUser1.bodyObj.token, 'channel1CName', false);
 
       const testList = requestChannelsList(testUser2.bodyObj.token);
       expect(testList.res.statusCode).toBe(OK);
@@ -241,55 +280,4 @@ describe('channels capabilities', () => {
     });
   });
 
-  /*
-  describe('channelsListallV1 test', () => {
-    test('authid is invalid - not an existing user', () => {
-      let testUser: user;
-      testUser = createTestUser('validemail@gmail.com', '123abc!@#', 'John', 'Doe');
-
-      expect(channelsListallV1(testUser.authUserId + 1)).toStrictEqual({ error: 'error' });
-    });
-
-    test('no channels in database', () => {
-      const testUser1 = requestAuthRegister('who@question.com', 'yourmumma', 'John', 'Smith');
-      expect(channelsListallV1(testUser1.authUserId)).toStrictEqual({ channels: [] });
-    });
-
-    test('return one channel', () => {
-      const testUser2 = requestAuthRegister('whom@question.com', 'youmumma', 'Joh', 'Smit');
-      const testChannel1 = createTestChannel(testUser2.authUserId, 'channelName', true);
-      expect(channelsListallV1(testUser2.authUserId)).toStrictEqual({
-        channels: [
-          {
-            channelId: testChannel1.channelId,
-            name: testChannel1.name,
-          }
-        ]
-      });
-    });
-
-    test('return multiple channels', () => {
-      const testUser3 = requestAuthRegister('who@questin.com', 'youumma', 'Jon', 'Smih');
-      const c1A = createTestChannel(testUser3.authUserId, 'channel1AName', false);
-      const c1B = createTestChannel(testUser3.authUserId, 'channel1BName', true);
-      const c1C = createTestChannel(testUser3.authUserId, 'channel1CName', false);
-
-      const expected = new Set([
-        {
-          channelId: c1A.channelId,
-          name: c1A.name,
-        },
-        {
-          channelId: c1B.channelId,
-          name: c1B.name,
-        },
-        {
-          channelId: c1C.channelId,
-          name: c1C.name,
-        },
-      ]);
-      const received = new Set(channelsListallV1(testUser3.authUserId).channels);
-      expect(received).toStrictEqual(expected);
-    });
-  }); */
 });

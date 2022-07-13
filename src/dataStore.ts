25a1ae1d4a8f9b7051490210c36ac1e5db2a05a6
Caitlyn Wong <z5360361@unsw.edu.au>
2022-07-14 03:53:52 +1000

    Fixed type errors for auth.ts, dataStore.ts and other.ts

---
+++ src/dataStore.ts
@@ -0,0 +1,89 @@
+interface user {
+  uId: number,
+  email: string,
+  password: string,
+  nameFirst: string,
+  nameLast: string,
+  handle: string,
+  globalPerms: number,
+};
+
+interface channelMember {
+  uId: number,
+  channelPerms: number,
+}
+
+interface dmMember {
+  uId: number,
+  dmPerms: number,
+}
+
+interface message {
+  messageId: number,
+  uId: number,
+  message: string,
+  timeSent: number,
+}
+
+interface channel {
+  channelId: number,
+  channelName: string,
+  isPublic: boolean,
+  members: channelMember[],
+  messages: message[],
+}
+
+interface token {
+  token: string,
+  uId: number,
+}
+
+interface dm {
+  dmId: number,
+  name: string,
+  members: dmMember[],
+  messages: message[],
+}
+
+interface database {
+  user: user[],
+  channel: channel[],
+  token: token[],
+  dm: dm[],
+}
+
+// YOU SHOULD MODIFY THIS OBJECT BELOW
+let data: database = {
+  user: [],
+  channel: [],
+  token: [],
+  dm: [],
+};
+
+// YOU SHOULDNT NEED TO MODIFY THE FUNCTIONS BELOW IN ITERATION 1
+
+/*
+Example usage
+    let store = getData()
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Rando'] }
+
+    names = store.names
+
+    names.pop()
+    names.push('Jake')
+
+    console.log(store) # Prints { 'names': ['Hayden', 'Tam', 'Rani', 'Giuliana', 'Jake'] }
+    setData(store)
+*/
+
+// Use get() to access the data
+function getData() {
+  return data;
+}
+
+// Use set(newData) to pass in the entire data object, with modifications made
+function setData(newData: database) {
+  data = newData;
+}
+
+export { getData, setData };
